92bcd0bf882322e5e0762778f56ec26104a530a5
priforjm <131368755+priforjm@users.noreply.github.com>
2023-09-08 21:39:25 +0800

    Add files via upload

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,21 +1,21 @@
 const CONFIG = {
   HOME_BANNER_ENABLE: true,
 
-  SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  SITE_CREATE_TIME: '2019-07-01', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到Jimmy的博客', url: 'https://huangjihao.com' },
+    { title: "点击前往Jimmy Huang's Blog¹", url: 'https://jimmyhjh.com' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HERO_TITLE_1: '分享编程',
-  HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
-  HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_1: '记录生活',
+  HERO_TITLE_2: '以及有的没的',
+  HERO_TITLE_3: 'HUANGJIHAO.COM',
+  HERO_TITLE_4: 'Notion Blog',
+  HERO_TITLE_5: "Jimmy Huang's Blog¹",
+  HERO_TITLE_LINK: 'https://jimmyhjh.com',
 
   // 英雄区显示三个置顶分类
   HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
@@ -23,20 +23,19 @@ const CONFIG = {
   HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HERO_RECOMMEND_POST_TAG: '推荐',
-  HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
+  HERO_RECOMMEND_POST_TAG: '',
+  HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: true, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   INFOCARD_GREETINGS: [
-    '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    'Hi,Jimmy Here',
+    '🎮 游戏爱好者',
+    '🎬 影视剧爱好者',
+    '📚 阅读爱好者',
+    '🖥️ 信息技术学习者',
+    '🕓 四分钟热度'
   ],
-  INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  INFO_CARD_URL: 'https://github.com/jimmyhuang22', // 个人资料底部按钮链接
 
   // 用户技能图标
   GROUP_ICONS: [
@@ -99,10 +98,10 @@ const CONFIG = {
   ],
 
   SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  SOCIAL_CARD_TITLE_1: '交流频道',
-  SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  SOCIAL_CARD_TITLE_3: '点击加入社群',
-  SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  SOCIAL_CARD_TITLE_1: 'Contact Me',
+  SOCIAL_CARD_TITLE_2: '',
+  SOCIAL_CARD_TITLE_3: '点击查看联系方式',
+  SOCIAL_CARD_URL: 'https://huangjihao.com/contact-me',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
